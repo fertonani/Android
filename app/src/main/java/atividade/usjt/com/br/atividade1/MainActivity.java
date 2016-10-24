865e1a70bb5ce3545dae2a4b0da3ca5c58c7cc0b
Guilherme <gui.fertonani@gmail.com>
2016-10-23 23:26:37 -0200

    Aula 14

--- app/src/main/java/atividade/usjt/com/br/atividade1/MainActivity.java
+++ app/src/main/java/atividade/usjt/com/br/atividade1/MainActivity.java
@@ -5,21 +5,38 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    public final static String MSG_EXTRA= "br.com.usjt.MSG_EXTRA";
+    public final static String AGENCIA = "br.com.usjt.AGENCIA";
+    public final static String CONTA = "br.com.usjt.CONTA";
+    public final static String SENHA = "br.com.usjt.SENHA";
+    private String agencia, conta, senha;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void enviarMensagem(View view){
 
-        Intent intent = new Intent(this, MensagemActivity.class);
-        EditText editText = (EditText)findViewById(R.id.mensagem);
-        String mensagem = editText.getText().toString();
-        intent.putExtra(MSG_EXTRA, mensagem);
-        startActivity(intent);
-
+    public void logar(View view) {
+        Intent intent = new Intent(this, MenuActivity.class);
+        EditText eagencia = (EditText)findViewById(R.id.agencia);
+        EditText econta = (EditText)findViewById(R.id.conta);
+        EditText esenha = (EditText)findViewById(R.id.senha);
+        agencia = eagencia.getText().toString();
+        conta = econta.getText().toString();
+        senha = esenha.getText().toString();
+        if (!agencia.equals("") | !conta.equals("") | !senha.equals("")) {
+            String msg = "Acesso permitido";
+            Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+            intent.putExtra(AGENCIA, agencia);
+            intent.putExtra(CONTA, conta);
+            intent.putExtra(SENHA, senha);
+            startActivity(intent);
+        } else {
+            String msg = "Agência e/ou Conta e/ou senha incorreto";
+            Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+        }
     }
 }
---
+++ app/src/main/java/atividade/usjt/com/br/atividade1/model/Login.java
@@ -0,0 +1,50 @@
+package atividade.usjt.com.br.atividade1.model;
+
+
+
+public class Login{
+	private String agencia, conta, senha;
+
+    public Login(String agencia, String conta, String senha) {
+        this.senha = senha;
+        this.agencia = agencia;
+        this.conta = conta;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getConta() {
+        return conta;
+    }
+
+    public void setConta(String conta) {
+        this.conta = conta;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Login)) return false;
+
+        Login login = (Login) o;
+
+        if (agencia != null ? !agencia.equals(login.agencia) : login.agencia != null) return false;
+        if (conta != null ? !conta.equals(login.conta) : login.conta != null) return false;
+        return senha != null ? senha.equals(login.senha) : login.senha == null;
+
+    }
+}
